f84eb57a90f3f6396196f09ac78de14d28378f9a
r-svp26 <riteshk6341@gmail.com>
2021-06-24 21:53:03 +0530

    Added: Percentage evaluation of Triplet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -4,10 +4,12 @@
 LIMIT=100
 
 # variable
-hh=0
-ht=0
-th=0
-tt=0
+hhh=0
+hht=0
+hth=0
+ttt=0
+tth=0
+tht=0
 flag=0
 
 declare -A result
@@ -16,38 +18,57 @@ while [ $flag -lt $LIMIT ]
 do
 	flip1=$((RANDOM%2))
 	flip2=$((RANDOM%2))
-	if [ $flip1 -eq 1 ] && [ $flip2 -eq 1 ]
+	flip3=$((RANDOM%2))
+	if [ $flip1 -eq 1 ] && [ $flip2 -eq 1 ] && [ $flip3 -eq 1 ]
 	then
-		let hh++
+		let hhh++
 
-	elif [ $flip1 -eq 1 ] && [ $flip2 -eq 0 ]
+	elif [ $flip1 -eq 1 ] && [ $flip2 -eq 1 ] && [ $flip3 -eq 0 ]
 	then 
-		let ht++
+		let hht++
 
-	elif [ $flip1 -eq 0 ] && [ $flip2 -eq 1 ]
+	elif [ $flip1 -eq 1 ] && [ $flip2 -eq 0 ] && [ $flip3 -eq 1 ]
 	then
-		let th++
+		let hth++
+
+
+	elif [ $flip1 -eq 0 ] && [ $flip2 -eq 0 ] && [ $flip3 -eq 0 ]
+        then
+                let ttt++
+
+        elif [ $flip1 -eq 0 ] && [ $flip2 -eq 0 ] && [ $flip3 -eq 1 ]
+        then
+                let tth++
 
 	else
-		let tt++
+		let tht++
 
 	fi
 	let flag++
 done
 
-hhPercentage=`echo $hh $LIMIT | awk '{hh=$1/$2*100} {print hh}'`
-htPercentage=`echo $ht $LIMIT | awk '{ht=$1/$2*100} {print ht}'`
-thPercentage=`echo $th $LIMIT | awk '{th=$1/$2*100} {print th}'`
-ttPercentage=`echo $tt $LIMIT | awk '{tt=$1/$2*100} {print tt}'`
+hhhPercentage=`echo $hhh $LIMIT | awk '{hhh=$1/$2*100} {print hhh}'`
+hhtPercentage=`echo $hht $LIMIT | awk '{hht=$1/$2*100} {print hht}'`
+hthPercentage=`echo $hth $LIMIT | awk '{hth=$1/$2*100} {print hth}'`
+tttPercentage=`echo $ttt $LIMIT | awk '{ttt=$1/$2*100} {print ttt}'`
+tthPercentage=`echo $tth $LIMIT | awk '{tth=$1/$2*100} {print tth}'`
+thtPercentage=`echo $tht $LIMIT | awk '{tht=$1/$2*100} {print tht}'`
 
-result[hh]=$hhPercentage
-result[ht]=$htPercentage
-result[th]=$thPercentage
-result[tt]=$ttPercentage
+
+
+result[hhh]=$hhhPercentage
+result[hht]=$hhtPercentage
+result[hth]=$hthPercentage
+result[ttt]=$tttPercentage
+result[tth]=$tthPercentage
+result[tht]=$thtPercentage
+
 
 echo ${result[@]} 
 
-echo "Combination of HH is $hhPercentage%"
-echo "Combination of HT is $htPercentage%"
-echo "Combination of TH is $thPercentage%"
-echo "Combination of TT is $ttPercentage%"
+echo "Combination of HHH is $hhhPercentage%"
+echo "Combination of HHT is $hhtPercentage%"
+echo "Combination of HTH is $hthPercentage%"
+echo "Combination of TTT is $tttPercentage%"
+echo "Combination of TTH is $tthPercentage%"
+echo "Combination of THT is $thtPercentage%"
