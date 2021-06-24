223bbd0944337d476838cc111ebabb4b3d5ed63f
r-svp26 <riteshk6341@gmail.com>
2021-06-24 17:20:42 +0530

    Added: Percentage evaluation of Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,13 +3,35 @@
 # constants
 HEAD=1
 TAIL=0
+LIMIT=150
 
 # variable
-flag=$((RANDOM%2))
+headCount=0
+tailCount=0
+totalCount=0
 
-if [ $flag -eq $HEAD ]
-then 
-	echo "Coin flips to Head"
-else
-	echo "Coin flips to Tail"
-fi
+declare -A result
+
+while [ $totalCount -lt $LIMIT ]
+do
+	flag=$((RANDOM%2))
+	if [ $flag -eq $HEAD ]
+	then
+		let headCount++
+
+	else
+		let tailCount++
+
+	fi
+	let totalCount++
+done
+
+headPercentage=`echo $headCount $LIMIT | awk '{h=$1/$2*100} {print h}'`
+tailPercentage=`echo $tailCount $LIMIT | awk '{t=$1/$2*100} {print t}'`
+
+result[h]=$headPercentage
+result[t]=$tailPercentage
+echo ${result[@]} 
+
+echo "Head percentage $headPercentage%" 
+echo "Tail percentage $tailPercentage%"
