57e1e852ad460166bed3d1fa78eeef4e73079dd7
r-svp26 <riteshk6341@gmail.com>
2021-06-24 16:13:23 +0530

    Added: To display Head or Tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
-echo "Welcome to Flip Coin Combination Program."
+# constants
+HEAD=1
+TAIL=0
+
+# variable
+flag=$((RANDOM%2))
+
+if [ $flag -eq $HEAD ]
+then 
+	echo "Coin flips to Head"
+else
+	echo "Coin flips to Tail"
+fi
